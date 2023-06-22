745ce934bf3f171f10f9dd129d297bf3c0d81b0c
Alexandre Podlewski <alexandre.podlewski@fabernovel.com>
2023-06-22 16:39:09 +0200

    Remove empty performance test

--- KeyboardManagerDemo/KeyboardManagerDemoTests/KeyboardManagerDemoTests.swift
+++ KeyboardManagerDemo/KeyboardManagerDemoTests/KeyboardManagerDemoTests.swift
@@ -21,12 +21,4 @@ class KeyboardManagerDemoTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
